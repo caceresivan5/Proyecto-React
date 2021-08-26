1b8b1507b914666affa81dd2be655a9e0e74e14f
Ivan Caceres <dg2.ivancaceres@gmail.com>
2021-08-25 21:16:55 -0300

    instalando y configurando la app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          MI PRIMER E-COMMERS EN REACT
+          "MI PRIMER E-COMMERS EN REACT"
         </p>
         
       </header>
