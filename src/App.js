535b6eede31fa959dad7d3b1353de522c9be6420
Ivan Caceres <dg2.ivancaceres@gmail.com>
2021-08-25 21:28:55 -0300

    Desafio 2

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           "MI PRIMER E-COMMERS EN REACT"
+          Desafio 2
         </p>
         
       </header>
